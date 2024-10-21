2e62d74cf9b37249b49e221fbf94174bd6de2668
Jaimin <jaiminjaiminpatel1@gmail.com>
2024-10-22 00:15:28 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+  function handleScrollAnimation() {
+    const elements = document.querySelectorAll('.fade-in');
+    
+    elements.forEach((element) => {
+      const elementTop = element.getBoundingClientRect().top;
+      const elementVisible = 150;
+      
+      if (elementTop < window.innerHeight - elementVisible) {
+        element.classList.add('appear');
+      } else {
+        element.classList.remove('appear');
+      }
+    });
+  }
+  
+  window.addEventListener('scroll', handleScrollAnimation);
+  window.addEventListener('load', handleScrollAnimation);
