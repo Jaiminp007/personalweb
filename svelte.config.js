1e4378f7de03ac4537f20f58a02e94f025668c45
Jaimin Patel <jaiminpatel@Jaimins-MacBook-Pro-2.local>
2026-01-05 19:18:03 -0500

    Config: Switch to adapter-node for Render deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-node';
 import { escapeSvelte, mdsvex } from 'mdsvex';
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
